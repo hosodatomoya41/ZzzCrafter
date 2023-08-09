39b66dc2daa6cf7d46475da93b2676b38207e90e
トモ <tomoyaa4819@gmail.com>
2023-08-09 17:55:10 +0900

    Add: sorceryの追加2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  authenticates_with_sorcery!
+  class user < applicationrecord
+  has_many :routines, through: :user_routines
+  has_many :user_routines, dependent: :destroy
+  has_many :sleep_issues, dependent: :destroy
+  has_many :sleep_records, dependent: :destroy
+  
+  validates :name, :email, :password_digest, presence: true
+  validates :email, uniqueness: true
+end
 end

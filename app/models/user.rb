e5deec0cde01cf794a6fab9f0fc77cc4cd62a6fa
トモ <tomoyaa4819@gmail.com>
2023-08-15 11:38:17 +0900

    Add: flash messageの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   
   validates :name, :email,  presence: true
   validates :email, uniqueness: true
-  validates :password, length: { minimum: 6 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 end

39a97ebb38f53ddc84fb2182f3fd7c848c4bc6e9
トモ <tomoyaa4819@gmail.com>
2023-08-22 11:34:58 +0900

    Remove: string型のrecommend_timeカラムの削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_08_073410) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_22_023404) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,7 +26,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_08_073410) do
   create_table "routines", force: :cascade do |t|
     t.string "name", null: false
     t.text "description", null: false
-    t.time "recommend_time", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

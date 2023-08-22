84e3faaea1c24e47c4c7afc95f31e084be987ea3
トモ <tomoyaa4819@gmail.com>
2023-08-22 11:33:11 +0900

    Add: seedファイルの追加1

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+routines = [
+  { name: "", description: "", recommend_time: "before0" },
+  { name: "", description: "", recommend_time: "before1" },
+  { name: "", description: "", recommend_time: "before3" }
+]
+
+routines.each do |routine|
+  routine.create(routine)
+end

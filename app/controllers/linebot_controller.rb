58932c5c08dc26c6d36f12ef3627406a6eb8f40e
トモ <tomoyaa4819@gmail.com>
2023-09-17 18:19:30 +0900

    Add: ユーザーからの入力に対する返答の追加(ベース)

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -9,6 +9,9 @@ class LinebotController < ApplicationController
     unless client.validate_signature(body, signature)
       return head :bad_request
     end
+    
+    validation_result = client.validate_signature(body, signature)
+    Rails.logger.info "Validation result: #{validation_result}" 
   
     events = client.parse_events_from(body)
     events.each do |event|
@@ -16,36 +19,38 @@ class LinebotController < ApplicationController
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
-          message = {
-            type: 'text',
-            text: event.message['text']
-          }
-          client.reply_message(event['replyToken'], message)
+          handle_message(event)
         end
       end
     end
   end
   
-  def send_message
-    line_user_id = params[:line_user_id]
-    message = params[:message]
-
-    client = Line::Bot::Client.new { |config|
-      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-    }
-
-    message = {
-      type: 'text',
-      text: message
-    }
-
-    response = client.push_message(line_user_id, message)
-    render json: { status: 'success' }
+  def calculate_recommend_time(bedtime)
+  # 最適な実践時間を計算（例：就寝時間の2時間前）
+    (bedtime - 2 * 60 * 60).strftime("%H:%M")
   end
   
   private
 
+  def handle_message(event)
+    line_user_id = event['source']['userId']
+    user = User.find_by(line_user_id: line_user_id)
+    Rails.logger.info "User ID: #{user&.id}"
+  
+    sleep_record = SleepRecord.where(user_id: user&.id).order("created_at DESC").first
+    Rails.logger.info "Sleep Record: #{sleep_record.inspect}"
+    bedtime = sleep_record&.bedtime
+
+    if event.message['text'] == "瞑想を追加しました"
+      recommend_time = calculate_recommend_time(bedtime)
+      message = {
+        type: "text",
+        text: "最適な実践時間は #{recommend_time} です"
+      }
+      client.reply_message(event['replyToken'], message)
+    end
+  end
+  
   def client
     @client ||= Line::Bot::Client.new { |config|
       config.channel_secret = ENV["LINE_CHANNEL_SECRET"]

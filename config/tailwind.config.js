199ef10de0c7dc49c1292b53ceb21899e769a53e
トモ <tomoyaa4819@gmail.com>
2023-09-18 21:49:11 +0900

    Mod: Tailwindの設定変更

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -5,5 +5,20 @@ module.exports = {
     './app/javascript/**/*.js',
   ],
   plugins: [require("daisyui")],
+  theme: {
+    extend: {
+      spacing: {
+        '15': '3.75rem',
+      },
+      width: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      height: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+  },
+  }
 };
 

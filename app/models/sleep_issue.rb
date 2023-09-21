2b2f3bd2855b419bda6180d8da77c4a1fa2e225b
トモ <tomoyaa4819@gmail.com>
2023-09-22 07:53:10 +0900

    Add: enumの追加

--- app/models/sleep_issue.rb
+++ app/models/sleep_issue.rb
@@ -3,4 +3,10 @@ class SleepIssue < ApplicationRecord
   has_many :issue_routines, dependent: :destroy
 
   validates :user_id, :issue_type, presence: true
+  
+  enum issue_type: {
+    night_owl: 0,
+    late_falling_asleep: 1,
+    waking_up_in_the_middle: 2
+  }
 end

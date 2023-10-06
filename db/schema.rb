c396bac71e91aaab614ea393ec43dc1e677ebb3d
トモ <tomoyaa4819@gmail.com>
2023-10-06 11:34:54 +0900

    sleep_issuesテーブルのnull制約解除、fly.tomlの編集、routine1.5hの対応

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_18_144245) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_05_131941) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,7 +33,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_18_144245) do
   end
 
   create_table "sleep_issues", force: :cascade do |t|
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
     t.integer "issue_type", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

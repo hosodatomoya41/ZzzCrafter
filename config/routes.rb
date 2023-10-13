f0d923f521fd2c2c422e5145099504f6e29377c1
トモ <tomoyaa4819@gmail.com>
2023-10-13 10:12:57 +0900

    fix: profileではなくusersと表示されるようにルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,15 @@ Rails.application.routes.draw do
   post '/callback' => 'linebot#callback'
   post '/send_line_message', to: 'linebot#send_message'
   
-  get '/profile', to: 'users#show', as: 'profile'
+  get '/users', to: 'users#show', as: 'users'
+  get '/users/edit', to: 'users#edit', as: 'users_edit'
   get '/profile/routine_records', to: 'users#routine_records', as: 'profile_routine_records'
   
-  resources :users, only: %i[new create edit update] do
+  resources :users, only: %i[new create update] do
     collection do
       get :recommend_routines
       post :recommend_routines
+      get :routine_records
     end
   end
   resources :routines, only: %i[index show]

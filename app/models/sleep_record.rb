8782bf843e93ec9967de79276184e0ebd8765cde
トモ <tomoyaa4819@gmail.com>
2023-10-24 18:14:06 +0900

    fix: linebot_controllerのリファクタリング

--- app/models/sleep_record.rb
+++ app/models/sleep_record.rb
@@ -7,4 +7,22 @@ class SleepRecord < ApplicationRecord
   validates :user_id, presence: true
 
   enum morning_condition: { good: 0, normal: 1, bad: 2 }
+  
+  CONDITION_MAPPING = {
+    '調子は良い' => :good,
+    '調子は普通' => :normal,
+    '調子は悪い' => :bad
+  }.freeze
+
+  def self.record_condition(user_id, received_text)
+    condition = CONDITION_MAPPING[received_text]
+    record = find_by(user_id: user_id, record_date: Date.yesterday, morning_condition: nil)
+
+    if record
+      record.update(morning_condition: condition, wake_up_time: Time.current)
+      '調子を記録しました。今日も一日頑張りましょう！'
+    else
+      '調子は記録済みです！今日も一日頑張りましょう！'
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,43 @@ class User < ApplicationRecord
   belongs_to :sleep_issue, optional: true
 
   validates :line_user_id, presence: true, uniqueness: true
+  
+  # 就寝時間とrecommend_timeに基づいて、推奨実践時間を計算
+  def calculate_time(bedtime, recommend_time)
+    offset = case recommend_time
+             when :before0 then 0
+             when :before1 then 1 * 60 * 60
+             when :before1_5 then 1.5 * 60 * 60
+             when :before3 then 3 * 60 * 60
+             when :before10 then 10 * 60 * 60
+             else 0
+             end
+    (bedtime - offset).strftime('%H:%M')
+  end
+
+  def register_routine(received_text)
+    routine = Routine.find_by(line_text: received_text)
+    existing_routine = UserRoutine.exists?(user_id: id, routine_id: routine.id, choose_date: Date.today)
+
+    return :existing_routine if existing_routine
+
+    UserRoutine.find_or_create_by(
+      user_id: id,
+      routine_id: routine.id,
+      choose_date: Date.today
+    )
+
+    SleepRecord.find_or_create_by(
+      user_id: id,
+      record_date: Date.today,
+      morning_condition: nil
+    )
+
+    if bedtime.nil?
+      return :no_bedtime
+    end
+
+    recommend_time = calculate_time(bedtime, routine.recommend_time.to_sym)
+    { status: :success, recommend_time: recommend_time }
+  end
 end

06d9b703253411bb9bee7bb5e72bebad44ff44ae
トモ <tomoyaa4819@gmail.com>
2023-11-17 10:05:45 +0900

    fix: ルーティーン一覧画面に睡眠の悩みを選択できるように修正

--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -2,12 +2,42 @@
 
 class RoutinesController < ApplicationController
   def index
-    @routines_before0 = Routine.where(recommend_time: 'before0')
-    @routines_before1 = Routine.where(recommend_time: %w[before1 before1_5])
-    @routines_before3 = Routine.where(recommend_time: %w[before3 before10])
+    if session[:user_id].present?
+    user = User.find(session[:user_id])
+    initialize_issue_type(user)
+    initialize_routines(user)
+    end
+
+    puts "issue_type= #{@current_issue_type}"
+    respond_to do |format|
+      format.html
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.replace('issue_content', template: 'routines/index')
+      end
+    end
   end
 
   def show
     @routine = Routine.find(params[:id])
+  end
+  
+  private
+  
+  def initialize_issue_type(user)
+    issue_type = params[:issue_type].presence || user&.sleep_issue&.issue_type
+    user.update_issue_type(issue_type) if issue_type.present?
+    @current_issue_type = issue_type
+    @selected_issue_point = user.selected_issue_point
+  end
+
+  def initialize_routines(user)
+    @routines = user.routines_based_on_issue
+    set_routines
+  end
+
+  def set_routines
+    @routines_before0 = @routines.where(recommend_time: 'before0')
+    @routines_before1 = @routines.where(recommend_time: %w[before1 before1_5])
+    @routines_before3 = @routines.where(recommend_time: %w[before3 before10])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,19 +44,6 @@ class UsersController < ApplicationController
     @grouped_sleep_records = SleepRecord.grouped_by_date(@year, @month, current_user.id)
   end
 
-  def recommend_routines
-    user = User.find(session[:user_id])
-    initialize_issue_type(user)
-    initialize_routines(user)
-
-    respond_to do |format|
-      format.html
-      format.turbo_stream do
-        render turbo_stream: turbo_stream.replace('issue_content', partial: 'users/recommend_routines')
-      end
-    end
-  end
-
   private
 
   def user_params
@@ -73,22 +60,4 @@ class UsersController < ApplicationController
     end
   end
 
-  def initialize_issue_type(user)
-    issue_type = params[:issue_type].presence || user&.sleep_issue&.issue_type
-    user.update_issue_type(issue_type) if issue_type.present?
-    @current_issue_type = issue_type
-    @selected_issue_point = user.selected_issue_point
-  end
-
-  def initialize_routines(user)
-    @issue_types = SleepIssue.issue_types.keys
-    @routines = user.routines_based_on_issue
-    set_routines_by_time
-  end
-
-  def set_routines_by_time
-    @routines_before0 = @routines.where(recommend_time: 'before0')
-    @routines_before1 = @routines.where(recommend_time: %w[before1 before1_5])
-    @routines_before3 = @routines.where(recommend_time: %w[before3 before10])
-  end
 end
